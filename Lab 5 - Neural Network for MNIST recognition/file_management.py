66fa607b514f86a25ec9c69e303e6d19abf0338b
Patrick Bedkowski <bedkowskisite@gmail.com>
2021-12-19 21:04:53 +0100

    Module for data operations

---
+++ Lab 5 - Neural Network for MNIST recognition/file_management.py
@@ -0,0 +1,24 @@
+from keras.datasets import mnist
+from keras.utils import np_utils
+
+
+def load_data():
+    return mnist.load_data()
+
+
+def preprocess_data(x, y, start=None, end=None):
+    x = x.reshape(x.shape[0], 28 * 28, 1)
+    x = x.astype("float32") / 255
+
+    # oneHotEncode output which is a number in range [0,9] into a vector of size 10
+    y = np_utils.to_categorical(y)
+    y = y.reshape(y.shape[0], 10, 1)
+
+    if start and end:
+        return x[start:end], y[start:end]
+    elif start:
+        return x[start:], y[start:]
+    elif end:
+        return x[:end], y[:end]
+    else:
+        return x, y
